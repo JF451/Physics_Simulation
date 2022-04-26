158ff5bc88d734633360be2d88e45f15f1f27aa5
Justin Fulkerson <justinfulkerson@Justins-MBP.lan>
2022-04-25 17:30:37 -0700

    Working on UI

---
+++ main.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+
+"""
+UI setup for the simulation
+"""
+window = Tk()
+window.title("Particle Simulation")
+window.geometry("700x500")
+
+
+
+#Buttons
+restart_button = Button(text="Restart")
+restart_button.grid(row=0, column=0)
+
+
+
+
+
+
+window.mainloop()
+
